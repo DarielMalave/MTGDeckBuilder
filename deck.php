502f7644ca2929d2f71ae9b2b9fea37277b16deb
DarielMalave <darielmalaveperez@gmail.com>
2022-03-23 15:08:38 -0500

    More progress on deck profile page (visuals and displaying all cards in their appropriate card type sections); restructuring some stylesheets

--- deck.php
+++ deck.php
@@ -1,27 +1,43 @@
-<?php require_once('templates/header.php'); ?>
+<?php require_once('templates/deck_header.php'); ?>
 
 <!-- creature, instant, artifact, enchantment, sorcery, planeswalker, land -->
-<h1>Standard Deck</h1>
+<div class="title_con">
+    <h1>Standard Deck</h1>
+</div>
 
-<h2>Creature</h2>
+<div class="title_con">
+    <h2>Creature<i class="fas-solid fa-circle-question"></i></h2>
+</div>
 <section id="card_container" class="creature"></section>
 
-<h2>Instant</h2>
+<div class="title_con">
+    <h2>Instant</h2>
+</div>
 <section id="card_container" class="instant"></section>
 
-<h2>Artifact</h2>
+<div class="title_con">
+    <h2>Artifact</h2>
+</div>
 <section id="card_container" class="artifact"></section>
 
-<h2>Enchantment</h2>
+<div class="title_con">
+    <h2>Enchantment</h2>
+</div>
 <section id="card_container" class="enchantment"></section>
 
-<h2>Sorcery</h2>
+<div class="title_con">
+    <h2>Sorcery</h2>
+</div>
 <section id="card_container" class="sorcery"></section>
 
-<h2>Planeswalker</h2>
+<div class="title_con">
+    <h2>Planeswalker</h2>
+</div>
 <section id="card_container" class="planeswalker"></section>
 
-<h2>Land</h2>
+<div class="title_con">
+    <h2>Land</h2>
+</div>
 <section id="card_container" class="land"></section>
 
 <script>
@@ -45,8 +61,8 @@ $.ajax({
     },
     success: function(response) {
         data_source = JSON.parse(decodeURIComponent(response));
-        displayList(data_source, document.getElementsByClassName("creature")[0], 10);
-        displayList(data_source, document.getElementsByClassName("instant")[0], 10);
+        //displayList(data_source, document.getElementsByClassName("creature")[0], 10);
+        //displayList(data_source, document.getElementsByClassName("instant")[0], 10);
         format_deck(data_source);
     }
 });
@@ -54,6 +70,8 @@ $.ajax({
 // let text = "Hello world, welcome to the universe.";
 // let result = text.includes("world");
 
+// also have function to sort cards by alphabetical order in each category
+// account for double-sided cards too
 function format_deck(data_source) {
     for (let i = 0; i < data_source.length; i++) {
         // clean card type name function here to take care of 
@@ -62,7 +80,33 @@ function format_deck(data_source) {
         if (data_source[i].type.includes("Creature")) {
             creature_cards.push(data_source[i]);
         }
+        else if (data_source[i].type.includes("Instant")) {
+            instant_cards.push(data_source[i]);
+        }
+        else if (data_source[i].type.includes("Artifact")) {
+            artifact_cards.push(data_source[i]);
+        }
+        else if (data_source[i].type.includes("Enchantment")) {
+            enchantment_cards.push(data_source[i]);
+        }
+        else if (data_source[i].type.includes("Sorcery")) {
+            sorcery_cards.push(data_source[i]);
+        }
+        else if (data_source[i].type.includes("Planeswalker")) {
+            planeswalker_cards.push(data_source[i]);
+        }
+        else if (data_source[i].type.includes("Land")) {
+            land_cards.push(data_source[i]);
+        }
     }
+
+    displayList(creature_cards, document.getElementsByClassName("creature")[0], creature_cards.length);
+    displayList(instant_cards, document.getElementsByClassName("instant")[0], instant_cards.length);
+    displayList(artifact_cards, document.getElementsByClassName("artifact")[0], artifact_cards.length);
+    displayList(enchantment_cards, document.getElementsByClassName("enchantment")[0], enchantment_cards.length);
+    displayList(sorcery_cards, document.getElementsByClassName("sorcery")[0], sorcery_cards.length);
+    displayList(planeswalker_cards, document.getElementsByClassName("planeswalker")[0], planeswalker_cards.length);
+    displayList(land_cards, document.getElementsByClassName("land")[0], land_cards.length);
 }
 
 function displayList(data_source, wrapper, rows_per_page) {
--- templates/deck_header.php
+++ templates/deck_header.php
@@ -7,22 +7,20 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>MTG Deck Builder</title>
-    <!-- Stylesheet referencing my custom styling -->
+    <title>Build Your Deck!</title>
+    <!-- STYLES -->
+    <link rel="stylesheet" href="styles/reset.css">
     <link rel="stylesheet" href="styles/styles.css">
-    <link rel="stylesheet" href="styles/gallery_styles.css">
-    <!-- Set up necessary to access Google Fonts -->
+    <link rel="stylesheet" href="styles/deck_styles.css">
+    <!-- GOOGLE FONTS -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Spline+Sans&display=swap" rel="stylesheet">
-    <!-- Set up necessary to access FontAwesome's assets -->
+    <!-- FONTAWESOME -->
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm" crossorigin="anonymous">
-    <!-- <script src="scripts/script.js"></script> -->
-    <!-- jquery cdn -->
+    <!-- JQUERY -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <script src="scripts/toggle_filter.js" defer></script>
-    <script src="scripts/displayList.js" defer></script>
-    <script src="scripts/gallery_events.js" defer></script>
+    
 </head>
 
 <body>
