5ec7dd12e4d17fa328ffd5661b4aa6da207f3662
DarielMalave <darielmalaveperez@gmail.com>
2022-03-24 14:12:21 -0500

    Completed edge case for deck profile if a card identifies as two card types like "Enchantment Creature" - deck now accepts just last card type ("Enchantment Creature" => "Creature")

--- deck.php
+++ deck.php
@@ -6,37 +6,61 @@
 </div>
 
 <div class="title_con">
-    <h2>Creature<i class="fas-solid fa-circle-question"></i></h2>
+    <h2>Creature</h2>
+    <div class="dropdown">
+        <img src="images/circle-question-solid.svg" class="icon">
+        <div class="dropdown-content">
+            <div class="info_text">
+                In Magic: The Gathering, creature is a permanent card type.
+                <br>
+                <br>
+                Flavorwise, creatures represent Warriors, Minions, Beasts,
+                and Monsters that serve the player, usually by fighting on their behalf.
+                Because almost all creatures can attack each turn to reduce an opponent's
+                life or block the opponent's attackers, creature cards are fundamental
+                to most deck strategies.
+                <br>
+                <br>
+                <a href="https://mtg.fandom.com/wiki/Creature" target="_blank">https://mtg.fandom.com/wiki/Creature</a>
+            </div>
+        </div>
+    </div>
 </div>
 <section id="card_container" class="creature"></section>
 
 <div class="title_con">
     <h2>Instant</h2>
+    <img src="images/circle-question-solid.svg" class="icon">
 </div>
 <section id="card_container" class="instant"></section>
 
 <div class="title_con">
     <h2>Artifact</h2>
+    <img src="images/circle-question-solid.svg" class="icon">
 </div>
 <section id="card_container" class="artifact"></section>
 
 <div class="title_con">
     <h2>Enchantment</h2>
+    <img src="images/circle-question-solid.svg" class="icon">
 </div>
 <section id="card_container" class="enchantment"></section>
 
 <div class="title_con">
     <h2>Sorcery</h2>
+    <img src="images/circle-question-solid.svg" class="icon">
 </div>
 <section id="card_container" class="sorcery"></section>
 
 <div class="title_con">
     <h2>Planeswalker</h2>
+    <img src="images/circle-question-solid.svg" class="icon">
 </div>
 <section id="card_container" class="planeswalker"></section>
 
 <div class="title_con">
     <h2>Land</h2>
+    <img src="images/circle-question-solid.svg" class="icon">
 </div>
 <section id="card_container" class="land"></section>
 
@@ -51,6 +75,11 @@ let sorcery_cards = [];
 let planeswalker_cards = [];
 let land_cards = [];
 
+let text = "Super Artifact Enchantment Creature - Human Soldier";
+const myArray = text.split("-");
+const newArray = myArray[0].split(" ");
+const trueType = newArray[newArray.length - 2];
+
 $.ajax({
     type: "POST",
     url: "process.php",
@@ -61,14 +90,9 @@ $.ajax({
     },
     success: function(response) {
         data_source = JSON.parse(decodeURIComponent(response));
-        //displayList(data_source, document.getElementsByClassName("creature")[0], 10);
-        //displayList(data_source, document.getElementsByClassName("instant")[0], 10);
         format_deck(data_source);
     }
 });
-
-// let text = "Hello world, welcome to the universe.";
-// let result = text.includes("world");
 
 // also have function to sort cards by alphabetical order in each category
 // account for double-sided cards too
@@ -77,25 +101,46 @@ function format_deck(data_source) {
         // clean card type name function here to take care of 
         // "artifact creature" and "enchantment creature"
         // it works by taking the last card type
-        if (data_source[i].type.includes("Creature")) {
+
+        // if (data_source[i].type.includes("-")) {
+        //     const myArray = data_source[i].type.split("-");
+        //     const newArray = myArray[0].split(" ");
+        //     const arrayLength = newArray.length - 2;
+        //     true_data_type = newArray[arrayLength];
+        // } else {
+        //     const newArray = data_source[i].type.split(" ");
+        //     const arrayLength = newArray.length - 1;
+        //     true_data_type = newArray[arrayLength];
+        // }
+
+        // https://www.w3schools.com/jsref/jsref_split.asp
+        let text = data_source[i].type;
+        let true_data_type;
+
+        if (text.includes('—') === true) {
+            const myArray = text.split("—");
+            const newArray = myArray[0].split(" ");
+            const arrayLength = newArray.length - 2;
+            true_data_type = newArray[arrayLength];
+        } else {
+            const newArray = text.split(" ");
+            const arrayLength = newArray.length - 1;
+            true_data_type = newArray[arrayLength];
+        }
+
+        if (true_data_type.includes("Creature")) {
             creature_cards.push(data_source[i]);
-        }
-        else if (data_source[i].type.includes("Instant")) {
+        } else if (true_data_type.includes("Instant")) {
             instant_cards.push(data_source[i]);
-        }
-        else if (data_source[i].type.includes("Artifact")) {
+        } else if (true_data_type.includes("Artifact")) {
             artifact_cards.push(data_source[i]);
-        }
-        else if (data_source[i].type.includes("Enchantment")) {
+        } else if (true_data_type.includes("Enchantment")) {
             enchantment_cards.push(data_source[i]);
-        }
-        else if (data_source[i].type.includes("Sorcery")) {
+        } else if (true_data_type.includes("Sorcery")) {
             sorcery_cards.push(data_source[i]);
-        }
-        else if (data_source[i].type.includes("Planeswalker")) {
+        } else if (true_data_type.includes("Planeswalker")) {
             planeswalker_cards.push(data_source[i]);
-        }
-        else if (data_source[i].type.includes("Land")) {
+        } else if (true_data_type.includes("Land")) {
             land_cards.push(data_source[i]);
         }
     }
@@ -116,8 +161,6 @@ function displayList(data_source, wrapper, rows_per_page) {
         return;
     }
 
-    // account for cases where page does have cards but not up to
-    // rows_per_page
     for (let i = 0; i < rows_per_page; i++) {
         let card_element = document.createElement('div');
         let card_image = document.createElement('img');
@@ -128,7 +171,6 @@ function displayList(data_source, wrapper, rows_per_page) {
         wrapper.appendChild(card_element);
     }
 }
-
 </script>
 
 <?php require_once('templates/footer.php'); ?>
